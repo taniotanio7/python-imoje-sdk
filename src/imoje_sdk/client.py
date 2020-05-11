5ffa943f5aa1108210563a9ba2e891f0962f36da
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2020-05-11 16:36:51 +0200

    base client implementation

---
+++ src/imoje_sdk/client.py
@@ -0,0 +1,34 @@
+import requests
+
+from imoje_sdk.enums import AllowedHTTPMetohds
+
+
+class Client:
+    BASE_URL = "https://api.imoje.pl/v1/merchant"
+
+    def __init__(self, merchant_id, auth_token):
+        super().__init__()
+        self.merchant_id = merchant_id
+        self._auth_token = auth_token
+
+    def _request(self, path: str, payload: dict, method: AllowedHTTPMetohds = "POST", headers=None, **kwargs):
+        _headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f"Bearer {self._auth_token}"
+        }
+
+        if headers:
+            _headers.update(headers)
+
+        url = f"{self.request_url}/{path}"
+
+        if method == AllowedHTTPMetohds.POST:
+            return requests.post(url, data=payload, headers=_headers, timeout=60, **kwargs)
+        else:
+            # GET
+            return requests.get(url, params=payload, headers=_headers, timeout=60, **kwargs)
+
+    @property
+    def request_url(self):
+        return f"{self.BASE_URL}/{self.merchant_id}"
---
+++ src/imoje_sdk/enums.py
@@ -0,0 +1,60 @@
+from enum import Enum, unique
+
+@unique
+class AllowedHTTPMetohds(Enum):
+    POST = "POST"
+    GET = "GET"
+
+
+@unique
+class TransactionStatus(Enum):
+    NOT_SEND = None  # Transaction wasn't send to the endpoint
+    NEW = 'new'  # Nowa, nieobsłużona transakcja
+    AUTHORIZED = 'authorized'  # Autoryzacja transakcji (karty)
+    PENDING = 'pending'  # Oczekiwanie na status
+    SUBMITTED = 'submitted'  # Wysłana do realizacji
+    REJECTED = 'rejected'  # Płatność odrzucona
+    SETTLED = 'settled'  # Transakcja zrealizowana
+    ERROR = 'error'  # Bład podczas realizowania transakcji
+    CANCELED = 'canceled'  # Transakcja anulowana
+
+
+@unique
+class PaymentMethod(Enum):
+    PAY_BY_LINK = "pbl"
+    CREDIT_CARD = "card"
+    BLIK = "blik"
+    ING = "ing"
+
+
+@unique
+class PayByLinkBank(Enum):
+    MTRANSFER = "mtransfer"  # mBank
+    BZWBK = "bzwbk"
+    PEAKO24 = "peako24"  # Pekao S.A
+    INTELIGO = "inteligo"
+    IPKO = "ipko"  # PKO BP
+    GETIN = "getin"
+    NOBLE = "noble"
+    IDEABANK = "ideabank"
+    CREDITAGRICOLE = "creditagricole"
+    T_MOBILE = "tmobile"
+    ALIOR = "alior"
+    PBS = "pbs"
+    MILLENNIUM = "millennium"
+    CITI = "citi"  # Citi Handlowy
+    BOS = "bos"  # BOŚ
+    BNPPARIBAS = "bnpparibas"
+    POCZTOWY = "pocztowy"  # Bank Pocztowy24
+    PLUS_BANK = "plusbank"
+    BS = "bs"  # Bank Spółdzielczy
+    BSPB = "bspb"  # Bank Spółdzielczy w Brodnicy
+    NEST = "nest"
+    ENVELO = "envelo"
+
+
+@unique
+class PayByCardMethod(Enum):
+    SECURE_3D = "ecom3ds"
+    ONECLICK = "oneclick"
+    RECURRING = "recurring"
